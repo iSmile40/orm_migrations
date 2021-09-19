77bf9527ae263e2b7294fc8c2cdc807ce003a3c8
Dmitry Nikitin <iSmile40@gmail.com>
2021-09-19 15:19:56 +0300

    Initial commit

---
+++ school/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    object_list = Student.objects.all()
+    context = {'object_list': object_list}
+
+    return render(request, template, context)
